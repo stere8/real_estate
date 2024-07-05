edfc49f719df80427509a5631f3bc3915a70e87f
unknown <twizoraph4247@gmail.com>
2024-07-06 00:14:16 +0200

    Adding contactus

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from .views import homepage
+from .views import homepage, contact
 
 
 urlpatterns = [
@@ -11,6 +11,7 @@ urlpatterns = [
     path('listings/', include('listings.urls')),
     path('accounts/', include('accounts.urls')),
     path('orders/', include('orders.urls')),  # Include orders URLs
+    path('contact/', contact, name='contact'),
     path('', homepage, name='homepage'),
 ]
 
--- real_estate/views.py
+++ real_estate/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'homepage.html')
+
+def contact(request):
+    return render(request,'contact_us.html')
